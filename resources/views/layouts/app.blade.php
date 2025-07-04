d63168a7fda37fed29d6fcea1df894b050f65a8c
Zura <dearisetiawan2004@gmail.com>
2025-07-04 19:58:35 +0800

    tambah floating wa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     {{-- Bootstrap CSS (5.3) --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
+
     {{-- Custom CSS --}}
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
@@ -20,7 +21,15 @@
             </div>
             @include('layouts.footer')
         </div>
+        
     </div>
+
+    <a href="https://wa.me/6281234567890?text=Halo,%20saya%20ingin%20menghubungi%20ARD%20Bali"
+    class="whatsapp-float" target="_blank" title="Hubungi ARD Bali">
+    <i class="bi bi-whatsapp"></i>
+    </a>
+
+
 
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
